56bb7877c4acb23bb6f00986d7b00b2a0b365d19
Neha03N <nikhadeneha4@gmail.com>
2024-06-05 00:50:36 +0530

    Updates

---
+++ precache-manifest.51c3625a49852fa4f2d2f30e5b6191f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "574c9e86a5e88a534516dc382b1e5fbd",
+    "url": "/RealTimeObjectDetection/index.html"
+  },
+  {
+    "revision": "3fbcbdb050b09124f4bd",
+    "url": "/RealTimeObjectDetection/static/css/main.af52fed7.chunk.css"
+  },
+  {
+    "revision": "15fac1b19d0d3d789a48",
+    "url": "/RealTimeObjectDetection/static/js/2.37bbdd3e.chunk.js"
+  },
+  {
+    "revision": "74f6145a49ba54ec59c98ae6568fb9bb",
+    "url": "/RealTimeObjectDetection/static/js/2.37bbdd3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fbcbdb050b09124f4bd",
+    "url": "/RealTimeObjectDetection/static/js/main.d225e326.chunk.js"
+  },
+  {
+    "revision": "154f08f21cee6862a8c5",
+    "url": "/RealTimeObjectDetection/static/js/runtime-main.dd9226d2.js"
+  }
+]);
